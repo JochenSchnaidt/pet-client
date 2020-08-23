a200f5a9bbcbfadb2e01f6867f0adfe640dbd957
jschnaidt <jochen.schnaidt@prodyna.com>
2020-08-23 18:48:13 +0200

    updated config

--- src/main/java/me/schnaidt/showcase/service/PetClient.java
+++ src/main/java/me/schnaidt/showcase/service/PetClient.java
@@ -19,20 +19,20 @@ public class PetClient {
   @Autowired
   private FeignPetsApi petsApi;
 
-  //  @Scheduled(initialDelay = 2000, fixedRate = 2000)
+  @Scheduled(initialDelay = 5000, fixedRate = 5000)
   public void getAll() {
     ResponseEntity<List<Pet>> responseEntity = petsApi.listPets(5);
     Optional.ofNullable(responseEntity.getBody()).ifPresent(f -> log.info(f.toString()));
   }
 
-  //  @Scheduled(initialDelay = 3000, fixedRate = 3000)
+  //@Scheduled(initialDelay = 3000, fixedRate = 3000)
   public void getSingleExisting() {
     ResponseEntity<Pet> responseEntity = petsApi.showPetById("3");
     Optional.ofNullable(responseEntity.getBody()).ifPresent(f -> log.info(f.toString()));
   }
 
-  //  @Scheduled(initialDelay = 4000, fixedRate = 4000)
-  public void netNonExisting() {
+  //@Scheduled(initialDelay = 4000, fixedRate = 4000)
+  public void getNonExisting() {
     try {
       ResponseEntity<Pet> responseEntity = petsApi.showPetById("6");
       Optional.ofNullable(responseEntity.getBody()).ifPresent(f -> log.info(f.toString()));
@@ -43,7 +43,6 @@ public class PetClient {
 
   @Scheduled(initialDelay = 2000, fixedRate = 4000)
   public void getConfigValue() {
-
     RestTemplate restTemplate = new RestTemplate();
     String value = restTemplate.getForObject("http://localhost:8080/greeting?name=Jochen", String.class);
     log.info(value);
