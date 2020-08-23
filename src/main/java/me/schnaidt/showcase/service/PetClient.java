9cbb99056fcae724f08924f4b7d3d2a3fbdfb1f2
jschnaidt <jochen.schnaidt@prodyna.com>
2020-08-23 18:32:20 +0200

    updated config

--- src/main/java/me/schnaidt/showcase/service/PetClient.java
+++ src/main/java/me/schnaidt/showcase/service/PetClient.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,9 +19,33 @@ public class PetClient {
   @Autowired
   private FeignPetsApi petsApi;
 
-  @Scheduled(initialDelay = 3000, fixedRate = 5000)
-  public void go() {
+  //  @Scheduled(initialDelay = 2000, fixedRate = 2000)
+  public void getAll() {
     ResponseEntity<List<Pet>> responseEntity = petsApi.listPets(5);
     Optional.ofNullable(responseEntity.getBody()).ifPresent(f -> log.info(f.toString()));
+  }
+
+  //  @Scheduled(initialDelay = 3000, fixedRate = 3000)
+  public void getSingleExisting() {
+    ResponseEntity<Pet> responseEntity = petsApi.showPetById("3");
+    Optional.ofNullable(responseEntity.getBody()).ifPresent(f -> log.info(f.toString()));
+  }
+
+  //  @Scheduled(initialDelay = 4000, fixedRate = 4000)
+  public void netNonExisting() {
+    try {
+      ResponseEntity<Pet> responseEntity = petsApi.showPetById("6");
+      Optional.ofNullable(responseEntity.getBody()).ifPresent(f -> log.info(f.toString()));
+    } catch (Exception e) {
+      log.error(e.getMessage());
+    }
+  }
+
+  @Scheduled(initialDelay = 2000, fixedRate = 4000)
+  public void getConfigValue() {
+
+    RestTemplate restTemplate = new RestTemplate();
+    String value = restTemplate.getForObject("http://localhost:8080/greeting?name=Jochen", String.class);
+    log.info(value);
   }
 }
